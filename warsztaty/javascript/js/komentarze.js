9c5a027756276647f9f85a712a3fcab639b91768
szczepaniakmichal <szczepaniakmichal@github.com>
2018-03-10 15:51:06 +0100

    zmienne w javascript 10 marzec

---
+++ warsztaty/javascript/js/komentarze.js
@@ -0,0 +1,11 @@
+// tak wygląda komentarz liniowy
+
+/*
+
+tutaj jest komentarz blokowy z kilku linii
+się kładający
+
+//  a tu nie wiem po co zagnieżdzę sobie liniowy komentarz
+
+*/
+
---
+++ warsztaty/javascript/js/zmienne.js
@@ -0,0 +1,17 @@
+var imie;
+var nazwisko = "Szczepaniak"
+var wiek = 30;
+
+console.log(imie);
+
+imie = "Michał";
+
+console.log(imie)
+
+// łączenie zmiennych
+var laczenieTekstu = "moje imie to: " + imie + " a nazwisko to: " + nazwisko;
+console.log(laczenieTekstu);
+
+
+
+
