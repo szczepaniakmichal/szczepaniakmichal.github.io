7c520aade6d1c89548b11304571d3b95796f4b0d
szczepaniakmichal <szczepaniakmichal@github.com>
2018-03-11 14:01:25 +0100

    JS math

---
+++ warsztaty/javascript/js/math.js
@@ -0,0 +1,10 @@
+// właściwości obiektu Math
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+// metody obiektu Math
+console.log(Math.cos(30));
+console.log(Math.random());
+console.log(Math.pow(3,3));
