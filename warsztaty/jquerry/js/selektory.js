97e2e73f64ec7c57860a52e5b47fa3c9cf3a5efe
szczepaniakmichal <szczepaniakmichal@github.com>
2018-03-18 12:02:59 +0100

    jQ selektory

---
+++ warsztaty/jquerry/js/selektory.js
@@ -0,0 +1,17 @@
+$(function(){
+    // wybieranie po tagu html
+    var wszystkieParagrafy = $("p");
+    //console.log(wszystkieParagrafy);
+    
+    // wybieranie kilku selektorów
+    var mojeSelektory = $("h1, h2, .paragraf");
+    //console.log(mojeSelektory);
+    
+    // wybieranie po id
+    var titleH1 = $("#first");
+    //console.log(titleH1);
+    
+    var pClass = $("div .paragraf");
+    console.log(pClass);
+    
+});
