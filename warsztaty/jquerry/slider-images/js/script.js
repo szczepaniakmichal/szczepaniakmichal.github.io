51926212a8fab30551fb731e82505d27a65bbe54
szczepaniakmichal <szczepaniakmichal@github.com>
2018-03-24 16:28:17 +0100

    slider skończony z Piotrkiem

--- warsztaty/jquerry/slider-images/js/script.js
+++ warsztaty/jquerry/slider-images/js/script.js
@@ -35,20 +35,44 @@
 
 
 // Kod Piotrka
+//defincja zmiennych
+    $(function(){
+        var slideShow = $(".slide-show");
+        var slideCount = $(".single-slide").length;
+        var slideWidth = 100/slideCount;
+        var slideIndex = 0;
 
-$(function(){
-    var slideShow = $(".slide-show");
-    var slideCount = $(".single-slide").length;
-    var slideWidth = 100/slideCount;
-    var slideIndex = 0;
+//szerokość "kiszy"
+        slideShow.css('width', slideCount*100 + '%');
 
+        slideShow.find('.single-slide').each(function(index){
+        $(this).css({'width':slideWidth + '%', 'margin-left': index * slideWidth +'%'});
+        });
+//przyciski ze strałakmi + podpięcie onclik
+        $('.prev-slide').click(function(){
+            slide(slideIndex - 1);
+        })
 
-    slideShow.css('width', slideCount*100 + '%');
+        $('.next-slide').click(function(){
+            slide(slideIndex + 1);
+        })
 
-    slideShow.find('.single-slide').each(function(index)){
-        $(this).css({'width':slideWidth + '%', 'margin-left': index * slideWidth +'%'})
+// funkcja odpowiada za przsunięcie "kliszy"
+        function slide(newSlideIndex){
+            if(newSlideIndex < 0 || newSlideIndex > slideCount - 1)
+            return;
+        
+            var slideCaption = slideShow.find('.slide-caption').eq(newSlideIndex);
+        
+            var newMarginLeft = (newSlideIndex * (-100)) + "%";
+        
+            slideCaption.hide();
+        
+            slideShow.animate({'margin-left': newMarginLeft}, 800, function(){
+                slideIndex = newSlideIndex;
+                slideCaption.fadeIn();
+        })
     }
-    
     
 });
 
