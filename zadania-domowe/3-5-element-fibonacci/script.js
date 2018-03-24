eba95e72a4498855a24b31404d82677d3f6d399a
szczepaniakmichal <szczepaniakmichal@github.com>
2018-03-24 08:56:48 +0100

    zadania Fibonacci

--- zadania-domowe/3-5-element-fibonacci/script.js
+++ zadania-domowe/3-5-element-fibonacci/script.js
@@ -1,28 +1,35 @@
-/*var entyElement = "";
+var fib = [];
 
-function fibo(){
+function fibo(n){
+    var i;
 
-var n = 100;
-var fib=1;
-var fib=1;
+    fib[0] = 0;
+    fib[1] = 1;
 
-for(var i=2; i<n; i++){
-    fib[i] = fib[i-1] + fib[i-2];
-}
-    console.log(fibo);
-}
-console.log(fibo);*/
-
-
-var i;
-var fib = []; // Initialize array!
-
-fib[0] = 0;
-fib[1] = 1;
-for(i=2; i<=10; i++)
+    for(var i=2; i<=n; i++)
 {
-    // Next fibonacci number = previous + one before previous
-    // Translated to JavaScript:
-    fib[i] = fib[i-2] + fib[i-1];
-    console.log(fib[i]);
+    fib[i] = fib[i-2] + fib[i-1];   
 }
+    console.log(fib[n]);
+    return;
+}
+fibo(15);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
