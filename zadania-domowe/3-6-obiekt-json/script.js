e3fa00e1e89a6317142f242f21f05ae217878fa2
szczepaniakmichal <szczepaniakmichal@github.com>
2018-03-24 08:56:58 +0100

    json

---
+++ zadania-domowe/3-6-obiekt-json/script.js
@@ -0,0 +1,27 @@
+//var jsonPracownicy = {
+//        "pracownicy":[
+//        {"firstName": "Krystian", "lastName": "Dziopa"}, 
+//        {"firstName": "Anna", "lastName": "Szapiel"},
+//        {"firstName": "Piotr", "lastName": "Żmuda"}
+//    ]
+//    
+//}
+//
+//console.log(jsonPracownicy.pracownicy); 
+//
+//jsonPracownicy.pracownicy.forEach( function( element, index ){
+//    console.log( "Index: " + index + " Imie: " + element.firstName + " Nazwisko: " + element.lastName);
+//});
+
+
+document.getElementsByTagName(‘section‘)[0].nodeType
+
+
+
+
+
+
+
+
+
+
