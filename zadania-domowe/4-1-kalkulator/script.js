4f1550aa84eb23c480b1da769f2c946d7e22cd0a
szczepaniakmichal <szczepaniakmichal@github.com>
2018-04-09 16:05:20 +0200

    kalkulator w drodze

---
+++ zadania-domowe/4-1-kalkulator/script.js
@@ -0,0 +1,29 @@
+function getVal(x){
+    document.getElementById('display').value += x;
+}
+
+function calculate(){
+    var display = document.getElementById('display').value;
+    document.getElementById('display').value = eval(display);
+}
+
+function cle() {
+    document.getElementById('display').value = "";
+}
+
+
+/*//--------------------------SCRIPT MONIKA
+
+function pobierz (x) {
+    document.getElementById("ekran").value += x ;
+
+}
+
+function oblicz () {
+    var ekran = document.getElementById("ekran").value;
+    document.getElementById("ekran").value = eval(ekran);
+}
+
+function czysc() {
+    document.getElementById("ekran").value = "";
+}*/
